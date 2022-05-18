6283e585500d592ef2d0d795d09be56aaf0bb323
Hamad Numan <78150971+hamad-9@users.noreply.github.com>
2022-05-18 18:30:20 +0300

    authentication state and protecting screens

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,23 +1,27 @@
 import AuthContent from "../components/Auth/AuthContent";
 import { login } from "../util/auth";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
+  const authCtx = useContext(AuthContext);
+
   async function loginHandler({ email, password }) {
     setIsAuthenticating(true);
     try {
-      await login(email, password);
+      const token = await login(email, password);
+      authCtx.authenticate(token);
     } catch (error) {
       Alert.alert(
         "Authentication Field",
         "Cloud not logged you in please check your credentials"
       );
+      setIsAuthenticating(false);
     }
-    setIsAuthenticating(false);
   }
 
   if (isAuthenticating) {
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,24 +1,28 @@
-import { useState } from "react";
-import AuthContent from "../components/Auth/AuthContent";
+import { useState, useContext } from "react";
 import { createUser } from "../util/auth";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
+import AuthContent from "../components/Auth/AuthContent";
 function SignupScreen() {
   // useState[(isAuthenticated, setIsAuthenticated)] = useState(false);
 
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
+  const authCtx = useContext(AuthContext);
+
   async function signupHandler({ email, password }) {
     setIsAuthenticating(true);
     try {
-      await createUser(email, password);
+      const token = await createUser(email, password);
+      authCtx.authenticate(token);
     } catch (error) {
       Alert.alert(
         "Authentication Field",
         "Could not create user, please check your input and try again later."
       );
+      setIsAuthenticating(false);
     }
-    setIsAuthenticating(false);
   }
 
   if (isAuthenticating) {
